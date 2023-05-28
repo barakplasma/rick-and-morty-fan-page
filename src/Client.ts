2707576c46e4a0d5e38309b46b11114b7cc4ddd0
Michael Salaverry <barakplasma@gmail.com>
2023-05-28 21:33:48 +0300

    Update Client.ts

--- src/Client.ts
+++ src/Client.ts
@@ -4,6 +4,7 @@ interface ICharacter {
   episode: Array<unknown>;
   name: string;
   id: number;
+  image: string;
   status: string;
   species: string;
   origin: { name: string };
@@ -25,4 +26,24 @@ export const residentsOfLocation = async (location: string) => {
   }, [] as ICharacter[]);
   unPopularCharacters.sort((a, b) => a.name.localeCompare(b.name));
   return unPopularCharacters;
+};
+
+const getCharacter = async (name: string) => {
+  return await fetcher(
+    `https://rickandmortyapi.com/api/character/?name=${name}`
+  );
+};
+
+export const charactersPopularity = async (characters: string[]) => {
+  const charactersData = await Promise.allSettled(characters.map(getCharacter));
+  return charactersData.flatMap(
+    (c) =>
+      c.status !== "rejected" && {
+        name: c.value.results[0].name,
+        popularity: c.value.results.reduce(
+          (acc: number, cur: { episode: [] }) => acc + cur.episode.length,
+          0
+        )
+      }
+  );
 };
